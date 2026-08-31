4beeff2b94650a93899f47a6371fe49f7f7d2b16
RobertoVelazquezGit <vmroberto@hotmail.es>
2026-08-31 18:09:23 +0200

    Comments

--- DataPipelineConceptsRanges/DataPipelineConceptsRanges.cpp
+++ DataPipelineConceptsRanges/DataPipelineConceptsRanges.cpp
@@ -29,7 +29,7 @@ concept Comparable = requires(T a, T b) {
 
 template<typename Container>
 concept NumericContainer = requires(Container c) {
-    typename Container::value_type;  // this is one of the requirements for a type to be considered Comparable  
+    typename Container::value_type;  // this is one of the requirements for a type to be considered NumericContainer  
     requires Numeric<typename Container::value_type>;
     { c.begin() } -> std::input_iterator;
     { c.end() } -> std::input_iterator;
@@ -43,10 +43,29 @@ concept StringLike = std::convertible_to<T, std::string>;
 
 class DataProcessor {
 public:
-    // Process numeric data with concepts-constrained templates
-    template<NumericContainer auto container>
-    auto analyzeData(decltype (container)& data) {
-        using ValueType = typename std::remove_cvref_t<decltype(container)>::value_type;
+    // Original
+    //// Process numeric data with concepts-constrained templates
+    //template<NumericContainer auto container>
+    //auto analyzeData(decltype (container)& data) {
+    //    using ValueType = typename std::remove_cvref_t<decltype(container)>::value_type;
+
+    //    // Use ranges for functional-style processing
+    //    auto positiveValues = data
+    //        | std::views::filter([](const auto& x) { return x > 0; });
+
+    //    auto squares = positiveValues
+    //        | std::views::transform([](const auto& x) { return x * x; });
+
+    //    // Calculate statistics
+    //    ValueType sum = std::accumulate(squares.begin(), squares.end(), ValueType{ 0 });
+    //    size_t count = std::distance(squares.begin(), squares.end());
+
+    //    return std::make_tuple(sum, count, count > 0 ? sum / count : 0);
+    //}
+
+    template<NumericContainer Container>
+    auto analyzeData(Container& data) {
+        using ValueType = typename Container::value_type;
 
         // Use ranges for functional-style processing
         auto positiveValues = data
@@ -56,11 +75,21 @@ public:
             | std::views::transform([](const auto& x) { return x * x; });
 
         // Calculate statistics
-        ValueType sum = std::accumulate(squares.begin(), squares.end(), ValueType{ 0 });
-        size_t count = std::distance(squares.begin(), squares.end());
+		ValueType sum = std::accumulate(  // Here stops being lazy and starts being eager, because we need to calculate the sum and count   
+            squares.begin(),
+            squares.end(),
+            ValueType{ 0 }
+        );
 
-        return std::make_tuple(sum, count, count > 0 ? sum / count : 0);
+		size_t count = std::distance(squares.begin(), squares.end());  // squares is not a container, so we need to use std::distance to get the count of elements  
+
+        return std::make_tuple(
+            sum,
+            count,
+            count > 0 ? sum / count : ValueType{ 0 }
+        );
     }
+
 
     // Concepts-constrained sorting function
     template<Comparable T>
@@ -71,6 +100,10 @@ public:
         else {
             std::ranges::sort(data, std::greater<T>{});
         }
+		// Classic STL algorithm std::sort(data.begin(), data.end());, std::ranges_sort with c++20,
+        // and std::ranges::sort with c++20, are all valid ways to sort a vector.
+        // The difference is that std::ranges::sort is a range-based algorithm that can be used with any range, not just containers.
+        // It also has a more modern interface that allows for more flexibility in specifying the sorting criteria.
     }
 
 	// Original
@@ -84,7 +117,7 @@ public:
     // Generic filter with concepts
     template<NumericContainer Container, typename Predicate>
     auto filterData(Container& data, Predicate pred)
-        requires std::predicate<Predicate, typename Container::value_type>
+        requires std::predicate<Predicate, typename Container::value_type>  // ToDo
     {
         auto filtered = data
             | std::views::filter(pred);
